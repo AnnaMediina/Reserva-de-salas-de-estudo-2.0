5dc83d89daee99ef6e82d90e451ad27f86e75893
Nicolas Freitas <n.freitas@unifesp.br>
2026-05-09 13:11:44 -0300

    tranforma reserva em sujeito, e o sujeito vai atualizar os observers

--- src/reserva.py
+++ src/reserva.py
@@ -1,15 +1,37 @@
 from datetime import datetime
 from usuario import Usuario
 from sala import Sala
+from observer import Subject
 
-class Reserva:
+class Reserva(Subject):
     def __init__(self, usuario: Usuario, sala: Sala, data_hora_inicio: datetime, data_hora_fim: datetime):
+        super().__init__() # Inicializa a lista de observers
         self.usuario = usuario
         self.sala = sala
         self.data_hora_inicio = data_hora_inicio
         self.data_hora_fim = data_hora_fim
-        
+        self.status = "Confirmada"
+        self.add_observer(self.usuario)
+
+    def modificar_horario(self, novo_inicio: datetime, novo_fim: datetime):
+        self.data_hora_inicio = novo_inicio
+        self.data_hora_fim = novo_fim
+
+        self.notify_push(
+            f"Horário da sala {self.sala.numero_sala} alterado.", 
+            {"novo_inicio": novo_inicio.strftime("%d/%m %H:%M"), "novo_fim": novo_fim.strftime("%d/%m %H:%M")}
+        )
+
+    def cancelar(self):
+        self.status = "Cancelada"
+        self.notify_pull()
+
+    def obter_estado(self):
+        return {
+            "sala": self.sala.numero_sala,
+            "status": self.status
+        }
+
     def __str__(self):
-        inicio_formatado = self.data_hora_inicio.strftime("%d/%m/%Y %H:%M")
-        fim_formatado = self.data_hora_fim.strftime("%H:%M")
-        return f"Reserva: {self.sala.numero_sala} por {self.usuario.nome} ({inicio_formatado} as {fim_formatado})"
+        inicio = self.data_hora_inicio.strftime("%d/%m %H:%M")
+        return f"Reserva [{self.status}]: {self.sala.numero_sala} por {self.usuario.nome} ({inicio})"
