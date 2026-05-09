0e4c02ad351c95f77c05abfac4502ab99c4d3cf5
Nicolas Freitas <n.freitas@unifesp.br>
2026-05-09 13:42:24 -0300

    correcao de bug

--- src/estrategia_reserva.py
+++ src/estrategia_reserva.py
@@ -7,31 +7,31 @@ from gerenciador_reservas import GerenciadorDeReservas
 class PoliticaDeReserva(ABC):
     @abstractmethod
     def verificar_conflito(self, sala: Sala, usuario: Usuario, inicio: datetime, fim: datetime) -> bool:
-        """Retorna True se houver conflito (não pode reservar), False se estiver liberado."""
         pass
 
 class PoliticaPrimeiroChegar(PoliticaDeReserva):
-    """Ninguém pode sobrepor a reserva de ninguém. Quem reservou primeiro, leva."""
     def verificar_conflito(self, sala: Sala, usuario: Usuario, inicio: datetime, fim: datetime) -> bool:
-        gerenciador = GerenciadorDeReservas()
+
+        gerenciador = GerenciadorDeReservas.get_instancia()
         reservas_da_sala = gerenciador.obter_reservas_por_sala(sala.numero_sala)
         
         for r in reservas_da_sala:
             if max(inicio, r.data_hora_inicio) < min(fim, r.data_hora_fim):
-                print(f"CONFLITO com a sala {sala.numero_sala} ja esta ocupada neste horario")
-                return True
+                print(f"[CONFLITO] A sala {sala.numero_sala} já está ocupada neste horário.")
+                return True 
         return False 
 
 class PoliticaPrioridadeDocente(PoliticaDeReserva):
-    """Professor pode derrubar a reserva de um aluno (dispara evento de cancelamento)."""
     def verificar_conflito(self, sala: Sala, usuario: Usuario, inicio: datetime, fim: datetime) -> bool:
-        gerenciador = GerenciadorDeReservas()
+
+        gerenciador = GerenciadorDeReservas.get_instancia()
         reservas_da_sala = gerenciador.obter_reservas_por_sala(sala.numero_sala)
+        
         for r in reservas_da_sala:
             if max(inicio, r.data_hora_inicio) < min(fim, r.data_hora_fim):
                 if isinstance(usuario, Professor) and not isinstance(r.usuario, Professor):
-                    print(f"PRIORIDADE do Professor {usuario.nome} sobrepondo reserva de {r.usuario.nome}.")
-                    r.cancelar()
+                    print(f"Prioridade do Professor {usuario.nome} sobrepondo reserva de {r.usuario.nome}.")
+                    r.cancelar() 
                     gerenciador.remover_reserva(r)
                     return False 
                 else:
