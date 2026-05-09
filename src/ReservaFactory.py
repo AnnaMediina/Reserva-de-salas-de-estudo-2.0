6a3d87e292f42fe39bcd0cf4ba1d5d8139e232aa
Nicolas Freitas <n.freitas@unifesp.br>
2026-05-09 13:30:02 -0300

    integra Strategy e Singleton na ReservaFactory

--- src/ReservaFactory.py
+++ src/ReservaFactory.py
@@ -2,9 +2,28 @@ from datetime import datetime
 from usuario import Usuario
 from sala import Sala
 from reserva import Reserva
+from estrategia_reserva import PoliticaDeReserva, PoliticaPrimeiroChegar
+from gerenciador_reservas import GerenciadorDeReservas
 
 class ReservaFactory:
-    @staticmethod
-    def criar_reserva(usuario: Usuario, sala: Sala, inicio: datetime, fim: datetime) -> Reserva:
+    _politica_atual = PoliticaPrimeiroChegar()
+
+    @classmethod
+    def definir_politica(cls, nova_politica: PoliticaDeReserva):
+        """Permite trocar a política em tempo de execução"""
+        cls._politica_atual = nova_politica
+
+    @classmethod
+    def criar_reserva(cls, usuario: Usuario, sala: Sala, inicio: datetime, fim: datetime):
+        tem_conflito = cls._politica_atual.verificar_conflito(sala, usuario, inicio, fim)
+        
+        if tem_conflito:
+            print(" Falha ao criar reserva devido a conflito de horário.\n")
+            return None
+        
+        
+        
         nova_reserva = Reserva(usuario, sala, inicio, fim)
+        GerenciadorDeReservas().adicionar_reserva(nova_reserva)
+        print(f"reserva criada com sucesso para {usuario.nome}.")
         return nova_reserva
