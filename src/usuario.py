fa97a579e7f448c66f0e417c0be054742c6562a8
Nicolas Freitas <n.freitas@unifesp.br>
2026-05-09 12:40:47 -0300

    implementa subclasses de Usuario (Aluno, Professor, Externo)

--- src/usuario.py
+++ src/usuario.py
@@ -1,9 +1,25 @@
-
 class Usuario:
-    def __init__(self,nome: str, vinculo: str):
+    def __init__(self, nome: str, vinculo: str):
         self.nome = nome
         self.vinculo = vinculo
-    
+
     def __str__(self):
         return f"{self.nome} ({self.vinculo})"
-    
+
+
+class Professor(Usuario):
+    def __init__(self, nome: str, identificador: str):
+        super().__init__(nome, "Professor")
+        self.identificador = identificador
+
+
+class Aluno(Usuario):
+    def __init__(self, nome: str, matricula: str):
+        super().__init__(nome, "Aluno")
+        self.matricula = matricula
+
+
+class Externo(Usuario):
+    def __init__(self, nome: str, cpf: str):
+        super().__init__(nome, "Externo")
+        self.cpf = cpf
