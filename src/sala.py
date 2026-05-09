37f54af61e90d3541f77d1005f143d42995f3494
Nicolas Freitas <n.freitas@unifesp.br>
2026-05-09 12:44:21 -0300

    implementa as subclasses concretas de cada tipo de sala com detalhes específicos de cada tipo

--- src/sala.py
+++ src/sala.py
@@ -10,3 +10,29 @@ class Sala(ABC):
         pass
     def __str__(self):
         return f"Sala {self.numero_sala} (Capacidade: {self.capacidade_total})"
+
+class Laboratorio(Sala):
+    def __init__(self, numero_sala: str, capacidade_total: int, equipamentos: list):
+        super().__init__(numero_sala, capacidade_total)
+        self.equipamentos = equipamentos
+
+    def obter_detalhes(self) -> str:
+        lista_equip = ", ".join(self.equipamentos) if self.equipamentos else "Nenhum"
+        return f"Laboratório, Equipamentos: {lista_equip}"
+    
+class EstudoIndividual(Sala):
+    def __init__(self, numero_sala: str, equipamentos: list = None):
+        super().__init__(numero_sala, capacidade_total=1)
+        self.equipamentos = equipamentos if equipamentos else []
+
+    def obter_detalhes(self) -> str:
+        return "Estudo Individual"
+    
+class EstudoEmGrupo(Sala):
+    def __init__(self, numero_sala: str, capacidade_total: int, quantidade_mesas: int, quantidade_quadros: int):
+        super().__init__(numero_sala, capacidade_total)
+        self.quantidade_mesas = quantidade_mesas
+        self.quantidade_quadros = quantidade_quadros
+
+    def obter_detalhes(self) -> str:
+        return f"Estudo em Grupo, {self.quantidade_mesas} Mesas, {self.quantidade_quadros} Quadros"
