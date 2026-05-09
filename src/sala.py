26a0e57be6a72d889abd3b3d49d87cad81e92149
Nicolas Freitas <n.freitas@unifesp.br>
2026-05-09 12:15:52 -0300

    A sala vai ser uma classe abstrata, e cada tipo de sala vai herdar a classe Sala como base.

--- src/sala.py
+++ src/sala.py
@@ -1,7 +1,12 @@
-class Sala:
+from abc import ABC, abstractmethod
+
+class Sala(ABC):
     def __init__(self, numero_sala: str, capacidade_total: int):
         self.numero_sala = numero_sala
         self.capacidade_total = capacidade_total
 
+    @abstractmethod
+    def obter_detalhes(self) -> str:
+        pass
     def __str__(self):
         return f"Sala {self.numero_sala} (Capacidade: {self.capacidade_total})"
