f233bb1aed6a3afa0c853febcadf459ffe41ac9c
Nicolas Freitas <n.freitas@unifesp.br>
2026-05-09 12:56:31 -0300

    agora coloquei os criadores para os 3 tipos (note que ele usa o metodo estatico pra criar um objeto)

--- src/sala_factory.py
+++ src/sala_factory.py
@@ -5,3 +5,15 @@ class SalaFactory(ABC):
     @abstractmethod
     def criar_sala(self, numero: str, **kwargs):
         pass
+
+class LaboratorioFactory(SalaFactory):
+    def criar_sala(self, numero: str, capacidade: int = 30, equipamentos: list = None):
+        return Laboratorio(numero, capacidade, equipamentos or [])
+
+class EstudoIndividualFactory(SalaFactory):
+    def criar_sala(self, numero: str, equipamentos: list = None):
+        return EstudoIndividual(numero, equipamentos or [])
+
+class EstudoEmGrupoFactory(SalaFactory):
+    def criar_sala(self, numero: str, capacidade: int = 6, mesas: int = 1, quadros: int = 1):
+        return EstudoEmGrupo(numero, capacidade, mesas, quadros)
