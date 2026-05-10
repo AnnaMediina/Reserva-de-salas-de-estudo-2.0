f3a148618b353b91c78b4cde4c92c8e1b8153dd9
Anna Clara Medina Roissmann <138173817+AnnaMediina@users.noreply.github.com>
2026-05-10 17:49:01 -0300

    mudança nos parâmetros do factory para criar as reservas corretamente

--- src/reserva_factory.py
+++ src/reserva_factory.py
@@ -5,7 +5,6 @@ from reserva import Reserva
 from estrategia_reserva import PoliticaDeReserva, PoliticaPrimeiroChegar
 from gerenciador_reservas import GerenciadorDeReservas
 
-# a factory cria os objetos do tipo Reserva ou os modifica, mas antes faz a verificação a partir da política de agendamento escolhida se a reserva pode ser feita ou nao.
 class ReservaFactory:
     _politica_atual = PoliticaPrimeiroChegar()
 
@@ -14,20 +13,20 @@ class ReservaFactory:
         cls._politica_atual = nova_politica
 
     @classmethod
-    def criar_reserva(cls, usuario: Usuario, sala: Sala, data: datetime, inicio: datetime, fim: datetime):
-        tem_conflito = cls._politica_atual.verificar_conflito(sala, usuario, data, inicio, fim)
+    def criar_reserva(cls, usuario: Usuario, sala: Sala, inicio: datetime, fim: datetime):
+        tem_conflito = cls._politica_atual.verificar_conflito(sala, usuario, inicio, fim)
         
         if tem_conflito:
             print(f"Falha ao criar reserva para {usuario.nome} devido a conflito de horário.")
             return None
-        nova_reserva = Reserva(usuario, sala, data, inicio, fim)
+        nova_reserva = Reserva(usuario, sala, inicio, fim)
         GerenciadorDeReservas.get_instancia().adicionar_reserva(nova_reserva)
         print(f" -> reserva criada com sucesso para {usuario.nome}.")
         return nova_reserva
     
     @classmethod
-    def modificar_reserva(cls, reserva: Reserva, nova_data: datetime, novo_inicio: datetime, novo_fim: datetime):
-        tem_conflito = cls._politica_atual.verificar_conflito(reserva.sala, reserva.usuario, nova_data, novo_inicio, novo_fim)
+    def modificar_reserva(cls, reserva: Reserva, novo_inicio: datetime, novo_fim: datetime):
+        tem_conflito = cls._politica_atual.verificar_conflito(reserva.sala, reserva.usuario, novo_inicio, novo_fim)
         if not tem_conflito:
             reserva.modificar_horario(novo_inicio, novo_fim)
         else:
