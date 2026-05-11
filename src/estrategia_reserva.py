53ee883605d84f5b5653f9c5468ab66b8cc77ae6
Anna Clara Medina Roissmann <138173817+AnnaMediina@users.noreply.github.com>
2026-05-11 11:12:15 -0300

    mudanças nos parâmetros das funções das classes

--- src/estrategia_reserva.py
+++ src/estrategia_reserva.py
@@ -11,33 +11,33 @@ class PoliticaDeReserva(ABC):
         pass
 
 class PoliticaPrimeiroChegar(PoliticaDeReserva):
-    def verificar_conflito(self, sala: Sala, usuario: Usuario, data: datetime, inicio: datetime, fim: datetime) -> bool:
+    def verificar_conflito(self, sala: Sala, usuario: Usuario, inicio: datetime, fim: datetime) -> bool:
 
         gerenciador = GerenciadorDeReservas.get_instancia()
         reservas_da_sala = gerenciador.obter_reservas_por_sala(sala.numero_sala)
         
         for reserva in reservas_da_sala:
-            if reserva.data_reserva.strftime("%d/%m/%Y") == data.strftime("%d/%m/%Y"):
-                if max(inicio, reserva.hora_inicio) < min(fim, reserva.hora_fim):
+            if reserva.data_hora_inicio.date().strftime("%d/%m/%Y") == inicio.strftime("%d/%m/%Y"):
+                if max(inicio, reserva.data_hora_inicio) < min(fim, reserva.data_hora_fim):
                     print(f"[CONFLITO] A sala {sala.numero_sala} já está ocupada neste horário por {reserva.usuario.nome}.")
                     return True 
         return False 
 
 class PoliticaPrioridadeDocente(PoliticaDeReserva):
-    def verificar_conflito(self, sala: Sala, usuario: Usuario, data: datetime, inicio: datetime, fim: datetime) -> bool:
+    def verificar_conflito(self, sala: Sala, usuario: Usuario, inicio: datetime, fim: datetime) -> bool:
 
         gerenciador = GerenciadorDeReservas.get_instancia()
         reservas_da_sala = gerenciador.obter_reservas_por_sala(sala.numero_sala)
         
         for reserva in reservas_da_sala:
-            if reserva.data_reserva.strftime("%d/%m/%Y") == data.strftime("%d/%m/%Y"):
-                if max(inicio, reserva.hora_inicio) < min(fim, reserva.hora_fim):
+            if reserva.data_hora_inicio.date().strftime("%d/%m/%Y") == inicio.strftime("%d/%m/%Y"):
+                if max(inicio, reserva.data_hora_inicio) < min(fim, reserva.data_hora_fim):
                     if isinstance(usuario, Professor) and not isinstance(reserva.usuario, Professor):
                         print(f"Prioridade do Professor {usuario.nome} sobrepondo reserva de {reserva.usuario.nome}.")
                         reserva.cancelar() 
                         gerenciador.remover_reserva(reserva)
                         return False 
-                else:
-                    print(f"[CONFLITO] Sala ocupada e sem prioridade suficiente.")
-                    return True 
+                    else:
+                        print(f"[CONFLITO] Sala ocupada e sem prioridade suficiente.")
+                        return True 
         return False
